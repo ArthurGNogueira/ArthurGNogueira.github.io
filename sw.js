3e9da00a6c5f463375090149744c775c68ce3d9d
Arthur G. Nogueira <35078082+ArthurGNogueira@users.noreply.github.com>
2023-01-27 18:23:59 -0300

    corrigindo google analytics
    corrigindo google analytics

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230127.175843';
+const cacheName = 'chirpy-20230127.182253';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
