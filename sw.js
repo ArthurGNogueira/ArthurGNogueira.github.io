913747f93d5cf56bef51265b0c8e26503638b35e
Arthur G. Nogueira <35078082+ArthurGNogueira@users.noreply.github.com>
2023-01-27 18:00:54 -0300

    Adicionando primeiro post
    primeiro post e configurações

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221220.013617';
+const cacheName = 'chirpy-20230127.175843';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
